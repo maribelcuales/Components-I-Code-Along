503dcaf9a314ce690e6329fbfde3aff1c0f0f6ed
maribelcuales <maribelcuales@gmail.com>
2020-05-03 20:27:27 +0800

    Added notes on components.

---
+++ index.js
@@ -0,0 +1,26 @@
+/////   Components I    /////
+// Instructor: Christina Gorton 
+
+// TK: https://learn.lambdaschool.com/web2/module/rec847sNXZX9CVDNl
+// youtube: https://www.youtube.com/watch?v=AJjYtcq1Uf4&amp=&feature=youtu.be
+
+// Notion Notes:
+// https://www.notion.so/Components-I-96090141608b468f94b070a78f49bbce
+
+// Code Along (codepen): 
+// Components I Solution:
+// https://codepen.io/maribelcuales/pen/ZEbyyxm
+// Components I starter (Bel's): 
+// https://codepen.io/maribelcuales/pen/mdewErP
+
+// Components Breakout Solution:
+// https://codepen.io/maribelcuales/pen/ExVXXEz
+
+// Project: 
+// https://github.com/LambdaSchool/Newsfeed-Components
+
+
+/////   Components    /////
+// - are reusable pieces of code that can be used to build elements sharing functionality and styling. 
+//- Components are the heart of any dynamic web application and JavaScript framework.
+// A component is made of several parts: HTML, CSS, or JavaScript brought together for reuse in a website or application.
